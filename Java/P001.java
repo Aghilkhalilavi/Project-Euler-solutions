ebe6b8c893d2e25c9fdaccc03fd7715d8cb345a5
Aghi <Aghilkh76@gmail.com>
2024-03-19 12:12:26 -0700

    P001

--- Java/P001.java
+++ Java/P001.java
@@ -1,8 +1,14 @@
- package Java;
+package Java;
 
 class P001 {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        int sum = 0;
+        for (int i = 1; i < 1000; i++)
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+
+            }
+        System.out.println(sum);
     }
 }
