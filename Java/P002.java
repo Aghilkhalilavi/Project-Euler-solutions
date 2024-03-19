54ce126ab094f47f49ffd4b71da14ea933328388
Aghi <Aghilkh76@gmail.com>
2024-03-19 14:20:03 -0700

    P002

---
+++ Java/P002.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class P002 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int x = 1;
+        int y = 2;
+        while (x <= 4000000) {
+            if (x % 2 == 0)
+                sum += x;
+            int z = x + y;
+            x = y;
+            y = z;
+        }
+        System.out.println(sum);
+    }
+}
